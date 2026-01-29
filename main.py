332e08dcfe9ab8c1cb9201f7c64be809dc733c4a
Aditya Sinsinwar <aditya@Adityas-MacBook-Air.local>
2026-01-29 10:59:24 +0530

    Learnt Post,get,request body

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello FastAPI"}
+class Item(BaseModel):
+    name:str
+    description:str = ""
+
+
+fake_db = {}
+
+@app.post("/items/{item_id}")
+def create_item(item_id: int, item: Item):
+    fake_db[item_id] = item
+    return {"message": "Item created", "item": item}
+
+
+@app.get("/items")
+def get_all():
+    return fake_db
+
+
+@app.get("/items/{item_id}")
+def get_items(item_id:int):
+    return fake_db.get(item_id,"Not Found :(")
